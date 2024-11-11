75d379de4c6e69cc28a3973c3dec589ef97bb01e
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2024-11-11 14:07:21 -0400

    Update Ejericicio_09_01.cpp

--- Ejericicio_09_01.cpp
+++ Ejericicio_09_01.cpp
@@ -2,4 +2,25 @@
 // Autor: Jhair Anderson Cortez Peña
 // Fecha creación: 10/11/2024
 // Número de ejercicio: 1
-// Problema planteado:
+// Problema planteado: Escribe una función recursiva que reciba un número entero positivo y
+devuelva la suma de sus dígitos
+#include <iostream>
+using namespace std;
+
+int sumaDeDigitos(int n) 
+{
+    if (n == 0) 
+    {
+        return 0;
+    }
+    return (n % 10) + sumaDeDigitos(n / 10);
+}
+
+int main() 
+{
+    int numero;
+    cout << "Introduce un numero entero positivo: ";
+    cin >> numero;
+    cout << "La suma de los digitos es: " << sumaDeDigitos(numero) << endl;
+    return 0;
+}
